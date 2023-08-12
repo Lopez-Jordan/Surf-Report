6143a35e02924d76eaeb26c9503544efb599bfab
Jordan Lopez <jordanlopezemail@gmail.com>
2023-08-11 22:20:54 -0700

    working on routes for homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,12 @@
 // homepage ('/') route here
 
 // route to api FOLDER  (mini router)
+
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.get('/');
+
+router.use('/api', apiRoutes);
+
+module.exports = router;
