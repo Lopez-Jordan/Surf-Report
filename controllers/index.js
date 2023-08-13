4c8dce7d8b0e2ebe6a421106a0b7bfa858b6f79e
Jordan Lopez <jordanlopezemail@gmail.com>
2023-08-13 15:30:40 -0700

    updated routes and dependencies

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const apiRoutes = require('./api/locationRoutes');
 const {Surfer, Location, SurferLocation} = require('../models');
 const {fetchLocationData, degreesToCardinal, avgWave } = require('../utils/convertLocation');
 
