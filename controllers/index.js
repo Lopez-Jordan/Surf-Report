a8149d5f43bead89c339930f693649e851457982
Jordan Lopez <jordanlopezemail@gmail.com>
2023-08-13 01:09:16 -0700

    created util function to to return object of dynamically generated surf location data given a models long and lat--still need to connect it in backend

--- controllers/index.js
+++ controllers/index.js
@@ -32,6 +32,49 @@ router.get('/', async (req, res) => {       // DONE (not tested)
 });
 
 
+// router.get('/', async (req, res) => {
+//     try {
+//       let allLocations = [];
+//       if (req.session.surferId) {
+//         allLocations = await Location.findAll({
+//           include: [
+//             {
+//               model: Surfer,
+//               through: {
+//                 model: SurferLocation,
+//                 where: {
+//                   surfer_id: req.session.surferId
+//                 }
+//               }
+//             }
+//           ]
+//         });
+//       }
+  
+//       const Locations = allLocations.map((el) => el.get({ plain: true })); // Array of plain objects
+  
+//       // Fetch Windy API data for each location and add properties
+//       const locationsWithWaveData = await Promise.all(
+//         Locations.map(async (location) => {
+//           const waveData = await fetchLocationData(location.lat, location.long);
+//           return {
+//             ...location,
+//             waveHeight: waveData.waveHeight,
+//             wavePeriod: waveData.wavePeriod,
+//             waveDirection: waveData.waveDirection
+//           };
+//         })
+//       );
+  
+//       res.status(200).render('homepage', {
+//         Locations: locationsWithWaveData,
+//         loggedIn: req.session.loggedIn
+//       });
+//     } catch (error) {
+//       res.status(400).json(error);
+//     }
+//   });
+
 router.use('/api', apiRoutes);
 
 module.exports = router;
