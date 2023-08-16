73ded49f84f554318783a71b417e6da552856adf
Jordan Lopez <jordanlopezemail@gmail.com>
2023-08-15 19:23:11 -0700

    updated the delete location and update description

--- public/js/location.js
+++ public/js/location.js
@@ -14,7 +14,10 @@ const newCardHandler = async (event) => {
            description: "",
           })
      });
-
+     if(response.ok){
+      alert('success!, Location added!');
+      document.location.replace('/');
+     }
    }catch (error){
      alert(error);
    }
@@ -25,6 +28,61 @@ const newCardHandler = async (event) => {
 
 
 
+ $(".share-btn").click(async function () {
+
+  const cardId = $(this).closest(".commentForm").data("card-id");
+  const newJournal = $(this).closest(".commentForm").find(".input-comment").val();
+  
+  await updateSurfJournal(cardId, newJournal);
+});
+
+async function updateSurfJournal(cardId, newJournal) {
+  try {
+    const response = await fetch(`/api/location/${cardId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ description: newJournal })
+    });
+
+    if (response.ok) {
+      alert("Success! Your journal has been added.");
+      document.location.replace('/');
+    } else {
+      alert("Problem updating card.");
+    }
+  } catch (error) {
+    alert("An error occurred.");
+  }
+}
+
+
+
+
+
+
+
+// Delete location card
+$(".delete-btn").click(async function () {
+  const cardId = $(this).closest(".form-group").data("card-id");
+  alert(cardId);
+  await deleteLocationCard(cardId);
+});
+
+async function deleteLocationCard(cardId) {
+  try {
+    const response = await fetch(`/api/location/${cardId}`, {
+      method: "DELETE"
+    });
+    if (response.ok){
+      alert("success deleting location!");
+      document.location.replace('/');
+    }
+  } catch (error) {
+    alert('there was a problem deleting the card');
+  }
+}
 
 
 
@@ -34,6 +92,12 @@ const newCardHandler = async (event) => {
 
 
 
-//event listener for delete location
-// $('#trash').on('submit', deleteLocation);
+
+
+
+
+
+
+
+
 
